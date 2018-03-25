f11c8ae52aa4ba91785a34c0f5dfd0a976ab5319
Eric Razor <eric.razorjr@gmail.com>
2018-03-25 18:09:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(days,holidayName = "Mother's Day"){
+function holidayCountdown(days = 20,holidayName = "Mother's Day"){
   return `It\'s #{days} days until #{holidayName}!`;
 }
